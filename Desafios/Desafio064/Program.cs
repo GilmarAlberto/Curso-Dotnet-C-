f7f5b0639bea45e9a260e31b3974d84b07b27d75
Gilmar Alberto <gilabreu00@gmail.com>
2024-03-14 18:06:01 -0300

    Desafios

---
+++ Desafios/Desafio064/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Altere o programa anterior para que ele aceite apenas números entre 0
+e 1000.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int num = rand.Next(0,1001);
+        int menor = int.MaxValue, maior = int.MinValue, soma = 0;
+
+        for(int i = 0; i < num; i++){
+            int j = rand.Next(0,1001);
+
+            Console.Write($"{j} ");
+            if(j < menor){
+                menor = j;
+            }
+            
+            if(j > maior){
+                maior = j;
+            }
+
+            soma += j;
+        }
+
+        Console.WriteLine($"\nQtdNum: {num}");
+        Console.WriteLine($"Menor: {menor}");
+        Console.WriteLine($"Maior: {maior}");
+        Console.WriteLine($"Soma: {soma}");
+     
+    }
+
+}
---
+++ Desafios/Desafio065/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Altere o programa de cálculo do fatorial, permitindo ao usuário calcular
+o fatorial várias vezes e limitando o fatorial a números inteiros positivos
+e menores que 16.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 0;
+
+        do{
+            int fat = 1;
+            
+            do
+            {
+                Console.WriteLine("Digite um número entre 0 e 16 para calcular o fatorial:");
+            } while (!int.TryParse(Console.ReadLine(), out num) || (num < -1 || num > 16));
+
+            if (num == -1){
+                break;
+            }
+
+            Console.WriteLine(num);
+
+            for(int i = 2; i <= num; i++){
+                    fat *= i;
+            }
+            
+            Console.WriteLine($"O fatorial {num}! é {fat}");    
+        }while(num != -1);
+    }
+}
