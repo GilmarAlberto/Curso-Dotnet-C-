a73baf0b955a130dc9e50b11f1d69826fd4ead7b
Gilmar Alberto <gilabreu00@gmail.com>
2024-04-06 01:42:36 -0300

    Desafios

---
+++ Desafios/Desafio067/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+.Altere o programa de cálculo dos números primos, informando, caso o
+número não seja primo, por quais número ele é divisível.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int  num   = rand.Next(2,1001);
+        bool primo = true, primeiro = true;
+        string divisores = "";
+
+        for(int i = 2; i <= (num/2); i++){
+            if(num % i == 0){
+
+                if(primeiro){
+                    Console.WriteLine($"O número {num} não é primo!");
+                    primeiro = false;
+                    primo = false;
+                }
+
+                divisores += $"{i} ";
+            }
+        }
+
+        if (primo){
+            Console.WriteLine($"O número {num} é primo!");
+        }
+        else{
+            Console.WriteLine($"O número {num} é divisível por {divisores}");
+        }
+             
+    }
+
+}
--- Desafios/Desafio067/obj/Debug/net8.0/Desafio067.AssemblyInfo.cs
+++ Desafios/Desafio067/obj/Debug/net8.0/Desafio067.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Desafio066")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Desafio067")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6c50a8563402ee1b634815bb0d43196541ab629")]
-[assembly: System.Reflection.AssemblyProductAttribute("Desafio066")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Desafio066")]
+[assembly: System.Reflection.AssemblyProductAttribute("Desafio067")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Desafio067")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
