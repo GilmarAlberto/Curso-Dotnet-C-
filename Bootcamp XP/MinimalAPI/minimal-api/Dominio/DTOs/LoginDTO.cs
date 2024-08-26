fcbb00fc00786c6a77cadd53234b1eb87581e79c
Gilmar Alberto <gilabreu00@gmail.com>
2024-08-26 08:03:28 -0300

    Bootcamp XP Minimal

--- Bootcamp XP/MinimalAPI/minimal-api/Dominio/DTOs/LoginDTO.cs
+++ Bootcamp XP/MinimalAPI/minimal-api/Dominio/DTOs/LoginDTO.cs
@@ -1,6 +1,6 @@
 namespace MinimalAPI.DTOs;
 public class LoginDTO
 {
-    public string Email {get; set;} = default;
-    public string Senha {get; set;} = default;
+    public string Email {get; set;} = string.Empty;
+    public string Senha {get; set;} = string.Empty;
 }
--- Bootcamp XP/MinimalAPI/minimal-api/Infraestrutura/Db/DbContexto.cs
+++ Bootcamp XP/MinimalAPI/minimal-api/Infraestrutura/Db/DbContexto.cs
@@ -1,6 +1,29 @@
+using MinimalAPI.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
+
 namespace MinimalAPI.Infraestrutura.Db;
 
-public class DbContexto
+public class DbContexto : DbContext 
 {
-    
+    private readonly IConfiguration _configuracaoAppSettings;
+
+    public DbContexto(IConfiguration configuracaoAppSettings)
+    {
+        _configuracaoAppSettings = configuracaoAppSettings;
+    }
+
+    public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var stringConexao = _configuracaoAppSettings.GetConnectionString("mysql")?.ToString();
+
+            if (!string.IsNullOrEmpty(stringConexao))
+            {
+                optionsBuilder.UseMySql(stringConexao, ServerVersion.AutoDetect(stringConexao));
+            }
+        }
+    }
 }
