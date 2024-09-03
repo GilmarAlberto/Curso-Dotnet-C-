06b9ad078d017493e775c1a99d83a6f753723ca9
Gilmar Alberto <gilabreu00@gmail.com>
2024-09-03 01:57:53 -0300

    Bootcamp XP MinimalAPI

--- Bootcamp XP/MinimalAPI/minimal-api/Program.cs
+++ Bootcamp XP/MinimalAPI/minimal-api/Program.cs
@@ -4,11 +4,15 @@ using MinimalAPI.Dominio.Servico;
 using Microsoft.AspNetCore.Mvc;
 using MinimalAPI.Infraestrutura.Db; // Para DbContexto
 using MinimalAPI.DTOs; // Para LoginDTO
+using MinimalAPI.Dominio.ModelViews;
+using MinimalAPI.Dominio.Entidades; 
 
-
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,10 +25,15 @@ builder.Services.AddDbContext<DbContexto>(options => {
 });
 
 var app = builder.Build();
+#endregion
 
-app.MapGet("/", () => "Hello World!");
+#region Home
+app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+#region Administradores
+
+app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
     if(administradorServico.Login(loginDTO) != null){
         return Results.Ok("Login com sucesso");
     }
@@ -32,9 +41,27 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
         return Results.Unauthorized(); 
     }
 });
+#endregion
 
+#region Veiculos
+ 
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) => {
 
+    var veiculo = new Veiculo{
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+    };
+
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+});
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
