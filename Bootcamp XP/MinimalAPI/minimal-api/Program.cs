35490beb1608367f2096f35ed40f9bad848d91a5
Gilmar Alberto <gilabreu00@gmail.com>
2024-09-04 15:04:03 -0300

    Organizando rotas por contexto no swagger

--- Bootcamp XP/MinimalAPI/minimal-api/Program.cs
+++ Bootcamp XP/MinimalAPI/minimal-api/Program.cs
@@ -28,7 +28,7 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home()));
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administradores
@@ -40,7 +40,7 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
     else{
         return Results.Unauthorized(); 
     }
-});
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -56,14 +56,14 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     veiculoServico.Incluir(veiculo);
 
     return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-});
+}).WithTags("Veiculos");
 
 app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) => {
 
     var veiculos = veiculoServico.Todos(pagina);
 
     return Results.Ok(veiculos);
-});
+}).WithTags("Veiculo");
 #endregion
 
 #region App
